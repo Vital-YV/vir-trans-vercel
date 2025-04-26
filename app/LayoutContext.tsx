5f5f3641ef0fe30e900b4f9afdcc9697002457ad
DanyaYashin <yashindan2000@gmail.com>
2025-04-26 14:03:21 +0300

    small fix

--- app/LayoutContext.tsx
+++ app/LayoutContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 
 // Create a context
 export const LayoutContext = createContext(null);
